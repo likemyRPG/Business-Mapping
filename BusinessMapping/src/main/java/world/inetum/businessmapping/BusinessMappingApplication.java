803c5dda87fd0f1bb3ad5464c1a0d797d23c9962
likemyRPG <vancampjochen@gmail.com>
2024-02-20 10:47:56 +0100

    Project Setup

---
+++ BusinessMapping/src/main/java/world/inetum/businessmapping/BusinessMappingApplication.java
@@ -0,0 +1,13 @@
+package world.inetum.businessmapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessMappingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessMappingApplication.class, args);
+    }
+
+}
---
+++ BusinessMapping/src/test/java/world/inetum/businessmapping/BusinessMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package world.inetum.businessmapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessMappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
