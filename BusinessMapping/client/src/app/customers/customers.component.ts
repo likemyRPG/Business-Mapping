6a161e5997c09da4cbab295df4e809d43143b354
likemyRPG <vancampjochen@gmail.com>
2024-02-28 11:23:36 +0100

    Add loading component and enhance sidebar styles
    Created a new loading component with related CSS, HTML, and tests to display a loading animation throughout the application. Sidebar's styles and HTML were also enhanced for a better user experience as well as improved tooltip displays. Implemented the loading component in the customers component to show loading state while fetching data.

--- BusinessMapping/client/src/app/customers/customers.component.ts
+++ BusinessMapping/client/src/app/customers/customers.component.ts
@@ -1,28 +1,32 @@
 // customers.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgIf, NgForOf, CommonModule } from '@angular/common';
 import { CustomerService } from '../services/customer.service'; // Import your service
-import { Customer } from '../models/Customer'; // Import your model
+import { Customer } from '../models/Customer';
+import {LoadingComponent} from "../loading/loading.component"; // Import your model
 
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [NgIf, NgForOf, CommonModule],
+  imports: [NgIf, NgForOf, CommonModule, LoadingComponent],
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.css']
 })
 export class CustomersComponent {
   customers: Customer[] = [];
+  loading = false;
 
-  constructor(private customerService: CustomerService) {
-    this.fetchCustomers();
+  constructor(private customerService: CustomerService) {}
+
+  ngOnInit() {
+    this.loadCustomers();
   }
 
-  fetchCustomers() {
-    // Replace with the actual call to the service
+  loadCustomers() {
+    this.loading = true;
     this.customerService.getAllCustomers().subscribe((data: Customer[]) => {
-      console.log(data);
       this.customers = data;
+      this.loading = false;
     });
   }
 }
