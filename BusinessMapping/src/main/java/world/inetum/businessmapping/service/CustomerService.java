75eb63a07f68b42415e3e4458d4c477397d8c5d5
likemyRPG <vancampjochen@gmail.com>
2024-03-13 09:00:32 +0100

    Remove customer creation and deletion methods
    The methods responsible for creating and deleting customers, both in the CustomerService class and CustomerController class, have been removed. As a result, functionalities related to creating and removing customers from the system are no longer available.

--- BusinessMapping/src/main/java/world/inetum/businessmapping/service/CustomerService.java
+++ BusinessMapping/src/main/java/world/inetum/businessmapping/service/CustomerService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import world.inetum.businessmapping.dto.CustomerDto;
 import world.inetum.businessmapping.entity.CustomerEntity;
 import world.inetum.businessmapping.repository.CustomerRepository;
 
@@ -28,21 +27,6 @@ public class CustomerService {
 
     public Mono<CustomerEntity> findById(String id) {
         return customerRepository.findById(id);
-    }
-
-    public Mono<CustomerEntity> create(CustomerDto customerDto) {
-        CustomerEntity customer = new CustomerEntity();
-        customer.setName(customerDto.getName());
-        customer.setSize(customerDto.getSize());
-        customer.setLocation(customerDto.getLocation());
-        customer.setNumberOfEmployees(customerDto.getNumberOfEmployees());
-        customer.setIndustry(customerDto.getIndustry());
-        customer.setRevenue(customerDto.getRevenue());
-        return customerRepository.save(customer);
-    }
-
-    public Mono<Void> delete(String id) {
-        return customerRepository.deleteById(id);
     }
 
     public Flux<CustomerEntity> findBySector(String sector) {
